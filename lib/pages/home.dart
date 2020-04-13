9ca8c5deabc31eb422a059c2c47ced58454fa2ca
Otavio Souza <otavio.silva133@etec.sp.gov.br>
2020-04-13 17:13:02 -0300

    card app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nubank_flutter/pages/widgets/card_app.dart';
 
 // widgets
 import 'package:nubank_flutter/pages/widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
+
   Widget build(BuildContext context) {
+    double _screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.purple,
       body: Stack(
         children: <Widget>[
-          MyAppBar()
+          MyAppBar(
+              showMenu: _showMenu,
+              onTap: () {
+                setState(() => _showMenu = !_showMenu);
+              }),
+          Positioned(
+              top: _screenHeight * .19,
+              height: _screenHeight * .45,
+              left: 0,
+              right: 0,
+              child: PageView(
+                physics: BouncingScrollPhysics(),
+                children: <Widget>[CardApp(), CardApp()],
+              ))
         ],
       ),
     );
--- lib/pages/widgets/my_app_bar.dart
+++ lib/pages/widgets/my_app_bar.dart
@@ -2,28 +2,41 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
+  final bool showMenu;
+  final VoidCallback onTap;
+
+  const MyAppBar({Key key, this.showMenu, this.onTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         SizedBox(height: MediaQuery.of(context).padding.top),
-        Container(
-          height: 200,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(
-                  'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
-                  height: 35,
-                  color: Colors.white),
-              SizedBox(width: 10),
-              Text(
-                'Otavio',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-        ),
+        GestureDetector(
+            onTap: onTap,
+            child: Container(
+                height: MediaQuery.of(context).size.height * .14,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.network(
+                            'https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-3.png',
+                            height: 35,
+                            color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          'Otavio',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Icon(!showMenu ? Icons.expand_more : Icons.expand_less)
+                  ],
+                ))),
       ],
     );
   }
