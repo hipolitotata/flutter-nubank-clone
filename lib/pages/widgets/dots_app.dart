7d6aa1a6d9a7a8092b6a1d0a0e320f18f743e2da
Otavio Souza <otavio.silva133@etec.sp.gov.br>
2020-04-13 18:47:32 -0300

    implemeted animation

--- lib/pages/widgets/dots_app.dart
+++ lib/pages/widgets/dots_app.dart
@@ -2,13 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DotsApp extends StatelessWidget {
-
   final double top;
   final int currentIndex;
 
   const DotsApp({Key key, this.top, this.currentIndex}) : super(key: key);
 
-  Color getColor(int index){
+  Color getColor(int index) {
     return index == currentIndex ? Colors.white : Colors.white30;
   }
 
@@ -18,21 +17,24 @@ class DotsApp extends StatelessWidget {
       top: top,
       child: Row(
         children: <Widget>[
-          Container(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
             width: 7,
             height: 7,
             decoration:
                 BoxDecoration(color: getColor(0), shape: BoxShape.circle),
           ),
           SizedBox(width: 5),
-          Container(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
             width: 7,
             height: 7,
             decoration:
                 BoxDecoration(color: getColor(1), shape: BoxShape.circle),
           ),
           SizedBox(width: 5),
-          Container(
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
             width: 7,
             height: 7,
             decoration:
